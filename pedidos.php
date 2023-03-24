327976e2a6f8a306d9879d8d3ea9edcc34591fdb
maucaruso <carusojr@outlook.com>
2023-03-24 07:14:42 -0300

    chore: Flyweight study case

---
+++ pedidos.php
@@ -0,0 +1,20 @@
+<?php
+
+use PHP\DesignPattern\Orcamento;
+use PHP\DesignPattern\Pedido;
+
+require 'vendor/autoload.php';
+
+$pedidos = [];
+$hoje = new \DateTimeImmutable();
+
+for ($i = 0; $i < 10000; $i++) {
+  $pedido = new Pedido();
+  $pedido->nomeCliente = md5((string) rand(1, 10000));
+  $pedido->orcamento = new Orcamento();
+  $pedido->dataFinalizacao = $hoje;
+  
+  $pedidos[] = $pedido;
+}
+
+echo memory_get_peak_usage();
