1beaa6e757e31d9265a40cf6d31e8846a2857494
maucaruso <carusojr@outlook.com>
2023-03-31 08:11:44 -0300

    chore: applying the Singleton Pattern

--- pdo.php
+++ pdo.php
@@ -4,9 +4,9 @@ use PHP\DesignPattern\PdoConnection;
 
 require 'vendor/autoload.php';
 
-$pdo = new PdoConnection('sqlite::memory');
+$pdo = PdoConnection::getInstance('sqlite::memory');
 // $pdo->query('');
 
-$pdo2 = new PdoConnection('sqlite::memory');
+$pdo2 = PdoConnection::getInstance('sqlite::memory');
 
 var_dump($pdo, $pdo2);
--- src/PdoConnection.php
+++ src/PdoConnection.php
@@ -4,5 +4,19 @@ namespace PHP\DesignPattern;
 
 class PdoConnection extends \PDO
 {
+  private static ?self $instance = null;
   
+  private function __construct($dns, $username = null, $password = null, $options = null)
+  {
+    parent::__construct($dns, $username, $password, $options);
+  }
+  
+  public static function getInstance($dns, $username = null, $password = null, $options = null): self
+  {
+    if (is_null(self::$instance)) {
+      self::$instance = new static($dns, $username, $password, $options);
+    }
+    
+    return self::$instance;
+  }
 }
